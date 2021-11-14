d45bb56dd84985fa5368119a08870267f6222d24
toonloncke <94291276+toonloncke@users.noreply.github.com>
2021-11-14 16:53:54 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Kip.")
+})
+basic.forever(function () {
+	
+})
